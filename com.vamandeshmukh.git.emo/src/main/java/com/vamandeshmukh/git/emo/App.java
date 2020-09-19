d50b48f9aa921c4de097b092303de24a89cd0b10
Vaman Deshmukh <vamandeshmukh@gmail.com>
2020-09-19 12:25:56 +0530

    Code commited.

---
+++ com.vamandeshmukh.git.emo/src/main/java/com/vamandeshmukh/git/emo/App.java
@@ -0,0 +1,14 @@
+package com.vamandeshmukh.git.emo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+
+		Employee emp = new Employee();
+		System.out.println(emp.toString());
+
+	}
+}
---
+++ com.vamandeshmukh.git.emo/src/main/java/com/vamandeshmukh/git/emo/Employee.java
@@ -0,0 +1,9 @@
+package com.vamandeshmukh.git.emo;
+
+public class Employee {
+	
+	int id;
+	String name;
+	double salary;
+
+}
